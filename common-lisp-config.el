f29052e7dafdb8dd9d8be784643a52111b8b10d4
Bozhidar Batsov <lordbad@gmail.com>
2011-02-15 17:39:36 +0200

    - made SBCL the default CL implementation

--- common-lisp-config.el
+++ common-lisp-config.el
@@ -2,11 +2,14 @@
 ;;; This file is part of the Emacs Dev Kit
 
 ;; a list of alternative Common Lisp implementations that can be
-;; used with SLIME
+;; used with SLIME. Note that their presence render
+;; inferior-lisp-program useless. This variable holds a list of
+;; programs and if you invoke SLIME with a negative prefix
+;; argument, M-- M-x slime, you can select a program from that list.
 (setq slime-lisp-implementations
-      '((clisp ("clisp" "-q"))
-        (cmucl ("cmucl" "-quiet"))
-        (sbcl ("sbcl" "--noinform") :coding-system utf-8-unix)))
+      '((sbcl ("sbcl" "--noinform") :coding-system utf-8-unix)
+        (clisp ("clisp" "-q"))
+        (cmucl ("cmucl" "-quiet"))))
 
 ;; delay SLIME loading until you actually need it
 (require 'slime-autoloads)
