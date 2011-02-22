e47e84f53b8feefe4140ef829eec10d539dcb925
Bozhidar Batsov <bozhidar.batsov@gmail.com>
2011-02-22 18:49:07 +0200

    perl config improvements

--- perl-config.el
+++ perl-config.el
@@ -1,26 +1,29 @@
 ;; cperl-mode - the best Perl mode for Emacs
 
 ;; use cperl-mode instead of perl-mode
-(mapc
- (lambda (pair)
-   (if (eq (cdr pair) 'perl-mode)
-       (setcdr pair 'cperl-mode)))
- (append auto-mode-alist interpreter-mode-alist))
+(defalias 'perl-mode 'cperl-mode)
 
-;; cperl-hairy affects all those variables, but I prefer
-;; a more fine-grained approach as far as they are concerned
-(setq cperl-font-lock t)
-(setq cperl-electric-lbrace-space t)
-(setq cperl-electric-parens nil)
-(setq cperl-electric-linefeed nil)
-(setq cperl-electric-keywords nil)
-(setq cperl-info-on-command-no-prompt t)
-(setq cperl-clobber-lisp-bindings t)
-(setq cperl-lazy-help-time 3)
 
-;; remove annoying trailing whitespace face(underscore)
-(setq cperl-invalid-face nil)
+(add-hook 'cperl-mode-hook 'my-cperl-mode-hook t)
+(defun my-cperl-mode-hook ()
+  (setq cperl-indent-level 4)
+  (setq cperl-continued-statement-offset 8)
+  ;; cperl-hairy affects all those variables, but I prefer
+  ;; a more fine-grained approach as far as they are concerned
+  (setq cperl-font-lock t)
+  (setq cperl-electric-lbrace-space t)
+  (setq cperl-electric-parens nil)
+  (setq cperl-electric-linefeed nil)
+  (setq cperl-electric-keywords nil)
+  (setq cperl-info-on-command-no-prompt t)
+  (setq cperl-clobber-lisp-bindings t)
+  (setq cperl-lazy-help-time 3)
 
-(global-set-key (kbd "C-h P") 'perldoc)
+  ;; if you want all the bells and whistles
+  ;; (setq cperl-hairy)
+
+  (set-face-background 'cperl-array-face nil)
+  (set-face-background 'cperl-hash-face nil)
+  (setq cperl-invalid-face nil))
 
 (provide 'perl-config)
