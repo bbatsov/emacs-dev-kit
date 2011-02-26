781140ea5426d3650fd179d45838d5978e4e3071
Bozhidar Batsov <bozhidar.batsov@gmail.com>
2011-02-26 04:19:55 +0200

    added subword-mode to scala-mode-hook

--- scala-config.el
+++ scala-config.el
@@ -5,6 +5,7 @@
 (add-to-list 'load-path (concat ext-dir "ensime_2.8.1-0.4.2/elisp/"))
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+(add-hook 'scala-mode-hook (lambda () (subword-mode)))
 ;; Make sure that your bin/server.sh has executable permission
 
 (provide 'scala-config)
