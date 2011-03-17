16fc002975a672a7cba726360eac65df6205b10d
Bozhidar Batsov <bozhidar.batsov@gmail.com>
2011-03-17 08:20:04 +0200

    - minor improvements to the Common Lisp configuration

--- common-lisp-config.el
+++ common-lisp-config.el
@@ -1,22 +1,21 @@
 ;;; Common Lisp
 ;;; This file is part of the Emacs Dev Kit
 
+;; Use SLIME from Quicklisp
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+
 ;; a list of alternative Common Lisp implementations that can be
 ;; used with SLIME. Note that their presence render
 ;; inferior-lisp-program useless. This variable holds a list of
 ;; programs and if you invoke SLIME with a negative prefix
 ;; argument, M-- M-x slime, you can select a program from that list.
 (setq slime-lisp-implementations
-      '((sbcl ("sbcl" "--noinform") :coding-system utf-8-unix)
-        (clisp ("clisp" "-q"))
-        (cmucl ("cmucl" "-quiet"))))
+      '((clisp ("clisp" "-q"))
+        (cmucl ("cmucl" "-quiet"))
+        (sbcl ("sbcl" "--noinform") :coding-system utf-8-unix)))
 
-;; delay SLIME loading until you actually need it
-(require 'slime-autoloads)
-
-;; slime-fancy is meta package containing all the most popular
-;; SLIME contrib packages
-(slime-setup '(slime-fancy))
+;; select the default value from slime-lisp-implementations
+(setq slime-default-lisp 'sbcl)
 
 (add-hook 'lisp-mode-hook 'lisp-coding-hook)
 (add-hook 'slime-repl-mode-hook 'interactive-lisp-coding-hook)
