bf5b57b5023edc42285ed0fe1ccd6b4a244898e8
Bozhidar Batsov <bozhidar.batsov@gmail.com>
2011-03-25 09:30:14 +0200

    added inf-ruby.el

--- ruby-config.el
+++ ruby-config.el
@@ -11,4 +11,12 @@
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
 
+(autoload 'run-ruby "inf-ruby"
+  "Run an inferior Ruby process")
+(autoload 'inf-ruby-keys "inf-ruby"
+  "Set local key defs for inf-ruby in ruby-mode")
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (inf-ruby-keys)))
+
 (provide 'ruby-config)
