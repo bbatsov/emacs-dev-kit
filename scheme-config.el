c3b8d0761b7d18a61b20436e968e2e51d4057311
Bozhidar Batsov <bozhidar.batsov@gmail.com>
2011-04-08 17:51:11 +0300

    simplified scheme config

--- scheme-config.el
+++ scheme-config.el
@@ -1,10 +1,6 @@
 ;;; Scheme
 ;;; This file is part of the Emacs Dev Kit
 
-;; Scheme - note that GDS is guile specific
-;; and that it's bundled with Guile in recent versions
-(require 'gds)
-
 (setq scheme-program-name "guile")
 (add-hook 'scheme-mode-hook 'lisp-coding-hook)
 
