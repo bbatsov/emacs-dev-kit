fa97a83280b15daaf4833b7c66079a42d4aeb3d2
Bozhidar Batsov <bozhidar.batsov@gmail.com>
2011-04-14 12:17:09 +0300

    - haml & scss support, ri integration

--- ruby-config.el
+++ ruby-config.el
@@ -15,8 +15,15 @@
   "Run an inferior Ruby process")
 (autoload 'inf-ruby-keys "inf-ruby"
   "Set local key defs for inf-ruby in ruby-mode")
+
+(require 'yari)
+
 (add-hook 'ruby-mode-hook
           '(lambda ()
-             (inf-ruby-keys)))
+             (inf-ruby-keys)
+             (local-set-key [f1] 'yari)))
+
+(require 'haml-mode)
+(require 'scss-mode)
 
 (provide 'ruby-config)
