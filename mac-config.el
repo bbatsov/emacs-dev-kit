5116b16f65caf6e0415cabd759c0acdb86050437
Bozhidar Batsov <bozhidar@Bozhidars-MacBook-Air.local>
2011-09-03 11:40:32 +0300

    remap option keys to controls on OSX

--- mac-config.el
+++ mac-config.el
@@ -1,3 +1,6 @@
+;; Emacs users obviously have little need for Command and Option keys,
+;; but they do need Meta and Control
 (setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'control)
 
 (provide 'mac-config)
