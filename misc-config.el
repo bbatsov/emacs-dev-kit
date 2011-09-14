2cf7c5181efb3136647fb2180b9f686268f68fdf
Bozhidar Batsov <bozhidar.batsov@gmail.com>
2011-09-14 08:05:03 +0300

    fixed graphviz startup problem

--- misc-config.el
+++ misc-config.el
@@ -280,7 +280,8 @@
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
 ;; graphviz-mode
-(require 'graphviz-dot-mode)
+(load-file (concat ext-dir "graphviz-dot-mode.el")) 
+
 
 (autoload 'find-file-in-project "find-file-in-project"
   "Find file in project." t)
